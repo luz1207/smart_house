538dc3d04169b73214d322666e3eabe1ae13a00c
cly <1466470706@qq.com>
2024-03-05 21:22:06 +0800

    update

--- webui/app.py
+++ webui/app.py
@@ -1,13 +1,116 @@
+import cv2
+import time
+import json
+import base64
 from flask import Flask
 from flask import render_template
+from flask import Flask, render_template, Response
+from flask import Flask, request, jsonify
+from tencentcloud.common import credential
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.iai.v20200303 import iai_client, models
+
+
 app = Flask(__name__)
+
+
+camera1 = cv2.VideoCapture(0)
+def read_usb_capture():
+    while 1:
+        ret,frame= camera1.read()
+        if not ret:break
+        #把获取到的图像格式转换(编码)成流数据，赋值到内存缓存中;
+        #主要用于图像数据格式的压缩，方便网络传输
+        frame = cv2.flip(frame,1)
+        ret1,buffer = cv2.imencode('.jpg',frame)
+        #将缓存里的流数据转成字节流
+        frame = buffer.tobytes()
+        #指定字节流类型image/jpeg
+        yield  (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    
+frame_interval=0.1   
+def detect_faces():  
+    ret, frame = camera1.read()
+    if not ret:
+        return 'no image'
+
+    base64_data = base64.b64encode(cv2.imencode('.jpg', frame)[1])
+    base64_code = base64_data.decode()
+
+
+    try:
+        httpProfile = HttpProfile()
+        httpProfile.endpoint = "iai.tencentcloudapi.com"
+        clientProfile = ClientProfile()
+        clientProfile.signMethod = "TC3-HMAC-SHA256"
+        cred = credential.Credential("AKID1KVku4AMUZhjdPyXu9CD8wY2KWaWK2hB","SoS1Rex8CFWktr9bswqnbHkgO4fRMEAs")
+        client = iai_client.IaiClient(cred, "ap-beijing", clientProfile)
+        req = models.DetectFaceRequest()
+
+        req.MaxFaceNum = 10
+        req.Image = base64_code
+        req.NeedFaceAttributes = 1
+        req.NeedQualityDetection = 1
+
+        resp = client.DetectFace(req)
+        json_data = resp.to_json_string()
+        
+        #将图像中的人脸标出来
+        face_num = len(json_data)
+        if face_num == 1:
+            image_w = json_data["ImageWidth"]
+            image_h = json_data["ImageHeight"] 
+            x = json_data["FaceInfos"]["X"]
+            y = json_data["FaceInfos"]["Y"]
+            width = json_data["FaceInfos"]["Width"]
+            height = json_data["FaceInfos"]["Height"]
+            if x + width >=image_w :
+                W = image_w
+            else:
+                W = x+width
+            if y + height >= image_h:
+                H = image_h
+            else:
+                H = y+height
+                
+            cv2.rectangle(frame, (x, y), (W, H), (0, 255, 0), 2)
+            # 将图像转换为 base64 编码的字符串
+            _, encoded_image = cv2.imencode('.jpg', frame)
+            image_data = base64.b64encode(encoded_image).decode()
+            response_data = {
+                'image': image_data,
+                'data_info': json_data
+            }
+
+            # 将数据转换为 JSON 格式的字符串
+            json_response = json.dumps(response_data)
+
+            yield f"data: {json_response}\n\n"
+            
+
+        # yield f"data: {json_data}\n\n"
+
+    except TencentCloudSDKException as err:
+        print(err)
+        
+    time.sleep(frame_interval)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/index/face')
-def face_regnize():
-    return render_template('face.html')
+@app.route('/video_feed0')
+def video_feed0():
+    return Response(read_usb_capture(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/detect_face')
+def detect_face():
+    return Response(detect_faces(), mimetype='text/event-stream')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080,debug=True)
 
--- webui/static/js/face_recognize.js
+++ webui/static/js/face_recognize.js
@@ -89,3 +89,22 @@ function getNowTime(){
     const seconds=date.getSeconds();
     return `${year}-${month}-${day} ${hour}:${minite}:${seconds<10?'0'+seconds:seconds}`
 }
+
+
+
+// 获取用于显示结果的容器
+const faceResultContainer = document.getElementById('faceResult');
+
+// 创建一个 EventSource 对象连接到服务器端的 /detect_face 路由
+const eventSource = new EventSource('/detect_face');
+
+// 当收到来自服务器端的消息时，更新页面上的人脸检测结果
+eventSource.onmessage = function(event) {
+    const jsonData = JSON.parse(event.data);
+    // 在这里可以根据需要处理人脸识别结果并更新页面内容
+    // 这里简单地将识别到的人脸信息显示在页面上
+    const faceInfoElement = document.createElement('pre');
+    faceInfoElement.textContent = JSON.stringify(jsonData, null, 2); // 使用 JSON.stringify 将 JSON 数据格式化并转换为字符串
+    faceResultContainer.innerHTML = ''; // 清空人脸检测结果容器
+    faceResultContainer.appendChild(faceInfoElement); // 将格式化后的 JSON 字符串添加到人脸检测结果容器中
+};
